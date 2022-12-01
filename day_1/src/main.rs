5a38ad0d881464647f46b64f76756c8fd185282b
Tillwege <tillwege@gmail.com>
2022-12-01 09:57:59 +0100

    day one

--- day_1/src/main.rs
+++ day_1/src/main.rs
@@ -1,3 +1,34 @@
-fn main() {
-    println!("Hello Day 1!");
+use std::{fs::File, io::Read, usize};
+
+fn main() -> std::io::Result<()> {
+    let mut file = File::open("input.txt")?;
+    let mut contents = String::new();
+    file.read_to_string(&mut contents)?;
+    
+
+    let mut elves: Vec<usize> = vec![0];
+
+    for line in contents.lines() {
+        if line.is_empty() {
+            elves.push(0);
+        } else {
+            let current_cals: usize = line.parse().unwrap();
+            
+            let current_elve = elves.last_mut().unwrap();
+            *current_elve += current_cals;
+        }
+    }
+
+    let mut largest_index: usize = 0;
+    let mut largest_cal_count: usize = 0;
+    for (index, cal_count) in elves.iter().enumerate() {
+        if *cal_count > largest_cal_count {
+            largest_index = index;
+            largest_cal_count = *cal_count
+        }
+    }
+
+    println!("Elve {} carries {} calories", largest_index, largest_cal_count);   
+
+    Ok(())
 }
