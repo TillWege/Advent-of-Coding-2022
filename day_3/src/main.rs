a6a0e001f19ec1a25138ec9545a488190ea8ce1a
Tillwege <tillwege@gmail.com>
2022-12-04 19:28:54 +0100

    day 3 part 2

--- day_3/src/main.rs
+++ day_3/src/main.rs
@@ -5,7 +5,7 @@ fn main() -> std::io::Result<()> {
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let mut total = 0;
+    let mut total_1 = 0;
 
     for line in contents.lines() {
         let (first_half, second_half) = line.split_at(line.chars().count() / 2);
@@ -13,14 +13,41 @@ fn main() -> std::io::Result<()> {
             if second_half.contains(c) {
                 let val = (c as u32) - (if c.is_uppercase() {38} else {96});
                 println!("{} = {}", c, val);
-                total += val;
+                total_1 += val;
                 break;
             }
         }        
     }
 
-    println!("Total: {}", total);
+    let mut line_1 = "";
+    let mut line_2 = "";
+    let mut total_2 = 0;
+    let mut group_count = 0;
+        
+    for line in contents.lines() {
+        if line_1.is_empty() {
+            line_1 = line
+        } else if line_2.is_empty() {
+            line_2 = line
+        } else {
+            for c in line.chars() {
+                if line_1.contains(c) && line_2.contains(c) {
+                    let val = (c as u32) - (if c.is_uppercase() {38} else {96});
+                    println!("{} = {}", c, val);
+                    total_2 += val;
+                    group_count += 1;
+                    break;
+                }
+            }
 
+
+            line_1 = "";
+            line_2 = "";
+        }
+    }
+
+    println!("Total (part 1): {}", total_1);
+    println!("Total (part 2): {} groups = {}", group_count, total_2);
 
     Ok(())
 }
