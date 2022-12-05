31da25a249dab36bea8b1ed17442834871c35b00
Tillwege <tillwege@gmail.com>
2022-12-05 13:55:39 +0100

    day 4 part 2

--- day_4/src/main.rs
+++ day_4/src/main.rs
@@ -1,12 +1,12 @@
-use std::{fs::File, io::Read};
+use std::{fs::File, io::Read, ops::RangeBounds};
 
 fn main() -> std::io::Result<()> {
     let mut file = File::open("./inputs/day4.txt")?;
     let mut contents = String::new();
     file.read_to_string(&mut contents)?;
 
-    let mut total = 0;
-
+    let mut total_1 = 0;
+    let mut total_2 = 0;
     for line in contents.lines() {
         let (first_half, second_half) = line.split_once(',').unwrap();
 
@@ -16,15 +16,24 @@ fn main() -> std::io::Result<()> {
         let range_2_lower: i32 = second_half.split('-').next().unwrap().parse().unwrap();
         let range_2_upper: i32 = second_half.split('-').last().unwrap().parse().unwrap();
 
-        // prüfen ob range 1 innerhalb von range 2 liegt
         if (range_1_lower >= range_2_lower) && (range_1_upper <= range_2_upper) {
-            total += 1;
+            total_1 += 1;
         } else if (range_2_lower >= range_1_lower) && (range_2_upper <= range_1_upper) {
-            total += 1;
+            total_1 += 1;
         }
+
+        if ((range_1_lower >= range_2_lower) && (range_1_lower <=range_2_upper)) || 
+            ((range_1_upper <= range_2_upper) && (range_1_upper >= range_2_lower)) {
+                total_2 += 1;
+        } else if ((range_2_lower >= range_1_lower) && (range_2_lower <= range_1_upper)) || 
+            ((range_2_upper <= range_1_upper) && (range_2_upper >= range_1_lower)) {
+                total_2 += 1;
+        } 
+
     }
 
-    println!("found {} included ranges", total);
+    println!("(part 1) found {} included ranges", total_1);
+    println!("(part 2) found {} included ranges", total_2);
 
     Ok(())
 }
